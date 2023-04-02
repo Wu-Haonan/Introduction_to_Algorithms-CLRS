77d445ef251ae5c051dcba7dbd7f855f705aec5b
Wu Haonan <63495803+Wu-Haonan@users.noreply.github.com>
2023-04-02 20:22:16 +0800

    Add files via upload

--- clrsPython/Lecture_2/Unimodal_Search.py
+++ clrsPython/Lecture_2/Unimodal_Search.py
@@ -1,1 +1,28 @@
+def Iterative_Unimodal_Search(A):
+    ## A[1...n] is a unimodal array
+    l = 0
+    r = len(A) - 1
+    m = int(((l+r)/2))
+    while A[m] < A[m-1] or A[m] < A[m+1]:
+        if A[m] < A[m-1]:
+            r = m-1
+        else:
+            l = m+1
+        m = int(((l + r) / 2))
+    return A[m]
 
+def Recursive_Unimodal_Search(A,l,r):
+    ## A[1...n] is a unimodal array
+    m = int(((l + r) / 2))
+    if A[m] > A[m-1] and A[m] > A[m+1]:
+        return A[m]
+    else:
+        if A[m] < A[m-1]:
+            return Recursive_Unimodal_Search(A, l, m-1)
+        else:
+            return Recursive_Unimodal_Search(A, m+1, r)
+
+
+A = [1,2,8,9,7,6,5,4,3,2,1]
+print(Iterative_Unimodal_Search(A))
+print(Recursive_Unimodal_Search(A,0,len(A)-1))
