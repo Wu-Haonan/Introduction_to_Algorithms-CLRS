7749ab50d361aa5f8d04ac0cf429625ee819a7ef
Wu Haonan <63495803+Wu-Haonan@users.noreply.github.com>
2023-04-03 15:39:44 +0800

    Add files via upload

--- clrsCpp/lecture_2/main.cpp
+++ clrsCpp/lecture_2/main.cpp
@@ -1,1 +1,11 @@
+# include <iostream>
 
+int Iterative_Unimodal_Search(int* array, int length);
+int Recursive_Unimodal_Search(int* array, int left, int right);
+
+int main(void) 
+{
+	int array[10] = { 1,2,3,4,5,6,10,9,8,7 };
+	std::cout << Iterative_Unimodal_Search(array, 10) << std::endl;
+	std::cout << Recursive_Unimodal_Search(array, 0, 9) << std::endl;
+}
